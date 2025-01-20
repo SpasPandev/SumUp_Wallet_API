a0f6f9ff67fe0c6f204d3459a243b108e19a2762
SpasPandev <sppan@abv.bg>
2025-01-20 20:33:32 +0200

    init

---
+++ src/test/java/com/example/sumup_wallet_api/SumUpWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sumup_wallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumUpWalletApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
